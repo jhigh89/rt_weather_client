84027613100f703020b4205fcebc02711fff5dc3
Jacob Hightower <jah795@nau.edu>
2021-05-10 01:22:04 -0700

    created program file with main function, testing

---
+++ program.py
@@ -0,0 +1,12 @@
+
+def main():
+
+    # Show the header
+    # Get the location request
+    # Convert plaintext over to data we can use
+    # Get report from the API
+    # Report the weather
+    print("Hello from weather main!")
+
+if __name__ == "__main__":
+    main()
