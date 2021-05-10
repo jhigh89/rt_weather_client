d7f99f40e8ca2b5b86c0ab625d630fd16f1bd854
Jacob Hightower <jah795@nau.edu>
2021-05-10 01:32:58 -0700

    testing! created show header, input for location

--- program.py
+++ program.py
@@ -1,12 +1,20 @@
 
 def main():
-
     # Show the header
+    show_header()
+
     # Get the location request
+    location_text = input("Which city's weather would you like to check? (e.g. San Francisco, CA, USA) \n")
+    print(f"You selected {location_text}")
     # Convert plaintext over to data we can use
     # Get report from the API
     # Report the weather
-    print("Hello from weather main!")
+
+def show_header():
+    print('--------------------------------')
+    print('---------Weather Client---------')
+    print('--------------------------------')
+    print('')
 
 if __name__ == "__main__":
     main()
